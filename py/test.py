14e885ea63c6f728da74fc1177ea583a2d290909
markwh1te <iamwh1temark@outlook.com>
2018-12-26 15:27:45 +0800

    add factorial

--- py/test.py
+++ py/test.py
@@ -1,17 +1,23 @@
 class Test(object):
     @staticmethod
+    def describe(sentence):
+        print(sentence)
+
+    @staticmethod
     def assert_equals(a, b):
         if len(a) != len(b):
             return 0
         for i, _ in enumerate(a):
             if a[i] != b[i]:
+                print("fail", a, "is not ", b)
                 return 0
+        print("success", a, b)
         return 1
 
     @staticmethod
     def assert_equal(a, b):
         if a != b:
-            print("fail", a, b)
+            print("fail", a, "is not ", b)
         else:
             print("success", a, b)
 
